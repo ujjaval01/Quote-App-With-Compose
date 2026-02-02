efce17a26d86a77e4067460c09062c2b6f86315a
Ujjaval Saini <sainiujvl@gmail.com>
2026-02-03 03:16:03 +0530

    Implement DataManager for asset loading
    - Create `DataManager` object to handle quote data management.
    - Implement `loadAssetsFromFile` to parse `quotes.json` from assets using GSON.
    - Add `isDataLoaded` state and `data` array to store retrieved quotes.

---
+++ app/src/main/java/com/uv/quotecomposeapp/DataManager.kt
@@ -0,0 +1,27 @@
+package com.uv.quotecomposeapp
+
+import android.content.Context
+import androidx.compose.runtime.mutableStateOf
+import com.uv.quotecomposeapp.model.Quote
+import com.google.gson.Gson
+
+object DataManager {
+
+    var data = emptyArray<Quote>()
+    var isDataLoaded = mutableStateOf(false)
+
+
+    fun loadAssetsFromFile(context: Context){
+        val inputStream = context.assets.open("quotes.json")
+        val size : Int = inputStream.available()
+        val buffer = ByteArray(size)
+        inputStream.read(buffer)
+        inputStream.close()
+        val json = String(buffer, Charsets.UTF_8)
+        val gson = Gson()
+        data = gson.fromJson(json, Array<Quote>::class.java)
+        isDataLoaded.value = true
+
+    }
+
+}
