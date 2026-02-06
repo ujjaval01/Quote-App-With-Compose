b8fd61657922712346ada6ec651aa396ca1167d7
Ujjaval Saini <sainiujvl@gmail.com>
2026-02-06 22:08:58 +0530

    Add DotsLoader and QuoteLoader components for enhanced UI feedback.

--- app/src/main/java/com/uv/quotecomposeapp/screens/QuoteListScreen.kt
+++ app/src/main/java/com/uv/quotecomposeapp/screens/QuoteListScreen.kt
@@ -1,5 +1,6 @@
 package com.uv.quotecomposeapp.screens
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -8,9 +9,12 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.uv.quotecomposeapp.R
 import com.uv.quotecomposeapp.model.Quote
 
 @Composable
@@ -20,15 +24,15 @@ fun QuoteListScreen(
 ) {
     Box {
         Column {
-
             Text(
-                text = "Quotes App",
+                text = "\"Quotes App\"",
                 textAlign = TextAlign.Center,
                 modifier = Modifier
-                    .padding(top = 24.dp, bottom = 8.dp)
+                    .padding(top = 28.dp, bottom = 8.dp)
                     .fillMaxWidth(),
                 style = MaterialTheme.typography.titleMedium,
-                fontFamily = FontFamily.Monospace
+                fontFamily = FontFamily.Monospace,
+                fontSize = 24.sp
             )
 
             QuoteList(
