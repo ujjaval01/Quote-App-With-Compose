c42c78a526c7352f25fd3aae01c76865d08d40b8
Ujjaval Saini <sainiujvl@gmail.com>
2026-02-06 22:08:26 +0530

    Add DotsLoader and QuoteLoader components for enhanced UI feedback.

--- app/src/main/java/com/uv/quotecomposeapp/MainActivity.kt
+++ app/src/main/java/com/uv/quotecomposeapp/MainActivity.kt
@@ -1,27 +1,57 @@
 package com.uv.quotecomposeapp
 
 import android.os.Bundle
+import android.text.Layout
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.animation.core.RepeatMode
+import androidx.compose.animation.core.animateFloat
+import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.rememberInfiniteTransition
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.unit.dp
+import androidx.core.view.WindowCompat
+import com.uv.quotecomposeapp.screens.DotsLoader
+import com.uv.quotecomposeapp.screens.PulseCircleLoader
 import com.uv.quotecomposeapp.screens.QuoteListScreen
+import com.uv.quotecomposeapp.screens.QuoteLoader
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        DataManager.loadAssetsFromFile(applicationContext)
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        CoroutineScope(Dispatchers.IO).launch {
+            delay(4500)
+            DataManager.loadAssetsFromFile(applicationContext)
+        }
         setContent {
-            MaterialTheme(
-//                colorScheme = DarkColorScheme,
-            ) {
-                enableEdgeToEdge()
+            SideEffect {
+                window.statusBarColor = Color.White.toArgb()
+                WindowCompat.getInsetsController(window, window.decorView)
+                    .isAppearanceLightStatusBars = true // dark icons
+            }
+            enableEdgeToEdge()
 //                QuoteListItems(Quote("Be yourself; everyone else is already taken.", "Theophrastus"), onClick = {})
                 App()
-            }
         }
     }
 }
@@ -30,10 +60,19 @@ class MainActivity : ComponentActivity() {
 fun App() {
     if(DataManager.isDataLoaded.value){
         QuoteListScreen(data = DataManager.data){
-
+        }
+    }
+    else{
+        Box(
+            modifier = Modifier.fillMaxSize(),
+            contentAlignment = Alignment.Center
+        ) {
+            DotsLoader()
         }
     }
 }
+
+
 
 // for making dark theme temporary
 private val DarkColorScheme = darkColorScheme(
