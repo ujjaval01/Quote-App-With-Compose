bcefb52e03b27afd6fbb4a7af5557cb11115a376
Ujjaval Saini <sainiujvl@gmail.com>
2026-02-12 01:38:59 +0530

    Rename Loaders.kt from screens to loader package for better organization.

--- app/src/main/java/com/uv/quotecomposeapp/MainActivity.kt
+++ app/src/main/java/com/uv/quotecomposeapp/MainActivity.kt
@@ -4,19 +4,11 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.darkColorScheme
-import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
 import androidx.core.view.WindowCompat
-import com.uv.quotecomposeapp.screens.DotsLoader
-import com.uv.quotecomposeapp.screens.QuoteDetailsScreen
-import com.uv.quotecomposeapp.screens.QuoteListScreen
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
@@ -28,7 +20,6 @@ class MainActivity : ComponentActivity() {
         WindowCompat.setDecorFitsSystemWindows(window, false)
         CoroutineScope(Dispatchers.IO).launch {
             delay(4500)
-            DataManager.loadAssetsFromFile(applicationContext)
         }
         setContent {
             SideEffect {
@@ -38,36 +29,9 @@ class MainActivity : ComponentActivity() {
             }
             enableEdgeToEdge()
 //                QuoteListItems(Quote("Be yourself; everyone else is already taken.", "Theophrastus"), onClick = {})
-                App()
+            MainScreen()
         }
     }
-}
-
-@Composable
-fun App() {
-    if(DataManager.isDataLoaded.value){
-
-        if(DataManager.currentPage.value == Pages.LISTING){
-            QuoteListScreen(data = DataManager.data){ quote ->
-                DataManager.switchPages(quote)
-            }
-        }else{
-            DataManager.currentQuote?.let { QuoteDetailsScreen(quote = it) }
-        }
-    }
-    else{
-        Box(
-            modifier = Modifier.fillMaxSize(),
-            contentAlignment = Alignment.Center
-        ) {
-            DotsLoader()
-        }
-    }
-}
-
-enum class Pages{
-    LISTING,
-    DETAIL
 }
 
 
