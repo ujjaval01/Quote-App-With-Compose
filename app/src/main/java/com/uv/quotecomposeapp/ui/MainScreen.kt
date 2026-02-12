7df0b0ffb38221c4438c5561b40dc085f212f42c
Ujjaval Saini <sainiujvl@gmail.com>
2026-02-12 21:36:02 +0530

    Add navigation support for new screens and update function parameters.

--- app/src/main/java/com/uv/quotecomposeapp/ui/MainScreen.kt
+++ app/src/main/java/com/uv/quotecomposeapp/ui/MainScreen.kt
@@ -5,9 +5,13 @@ import androidx.compose.runtime.Composable
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.compose.*
 import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Modifier
+import com.uv.quotecomposeapp.ui.screens.AboutScreen
 import com.uv.quotecomposeapp.ui.screens.FavoritesScreen
 import com.uv.quotecomposeapp.ui.screens.HomeScreen
+import com.uv.quotecomposeapp.ui.screens.PrivacyPolicyScreen
 import com.uv.quotecomposeapp.ui.screens.QuotesScreen
 import com.uv.quotecomposeapp.ui.screens.SettingsScreen
 import com.uv.quotecomposeapp.viewmodel.QuoteViewModel
@@ -17,12 +21,18 @@ fun MainScreen() {
 
     val navController = rememberNavController()
     val viewModel: QuoteViewModel = viewModel()
+    val favorites by viewModel.favorites.observeAsState(emptyList())
+
 
     Scaffold(
         bottomBar = {
-            BottomNavigationBar(navController)
+            BottomNavigationBar(
+                navController = navController,
+                favoriteCount = favorites.size   // ✅ yaha change
+            )
         }
-    ) { padding ->
+    )
+    { padding ->
 
         NavHost(
             navController = navController,
@@ -37,16 +47,26 @@ fun MainScreen() {
             composable("quotes?category={category}") { backStackEntry ->
                 val category =
                     backStackEntry.arguments?.getString("category")
-                QuotesScreen(category, viewModel)
+                QuotesScreen(category, viewModel, navController)
             }
 
             composable("favorites") {
-                FavoritesScreen(viewModel)
+                FavoritesScreen(viewModel, navController)
             }
 
             composable("settings") {
-                SettingsScreen()
+                SettingsScreen(navController = navController, viewModel = viewModel
+                )
             }
+
+            composable("about") {
+                AboutScreen(navController)
+            }
+
+            composable("privacy") {
+                PrivacyPolicyScreen(navController)
+            }
+
         }
     }
 }
